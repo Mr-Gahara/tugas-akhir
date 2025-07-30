edbe4453fb2152d5b4c1286eed134b168650ca5a
Mr-Gahara <zorrokeren3@gmail.com>
2025-07-30 13:08:05 +0700

    fix: package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This is the correct key for this version of Next.js 14
-  serverComponentsExternalPackages: [
-    '@prisma/client',
-    '@prisma/extension-accelerate',
-    '@uploadthing/shared',
-  ],
   images: {
     domains: ["utfs.io"],
   },
