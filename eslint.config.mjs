d8265a126205be2ead2032b004d9123cf0d9e70c
Mr-Gahara <zorrokeren3@gmail.com>
2025-07-30 11:53:32 +0700

    chore: finally correct all configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc/dist/eslintrc.cjs";
 
 const __filename = fileURLToPath(import.meta.url);
